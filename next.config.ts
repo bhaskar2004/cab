e22cc66ecad5f3a3fa5eb3654842e9e6333b1a75
bhaskar2004 <bhaskarbhaskar4794@gmail.com>
2026-01-04 23:06:26 +0530

    fix: disable lint on build via CLI flag and remove invalid config

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default withNextIntl(nextConfig);
